9b5813a8578bbefae61db3946e1c930f8993e641
kappafon <goran.rihter@gmail.com>
2019-07-28 17:43:24 +0200

    Updates

---
+++ precache-manifest.b72deeddfa92dec3f753d88ddc0af3e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1507f7ac4c3e7bfd5e78196d06952a",
+    "url": "/pamflet/index.html"
+  },
+  {
+    "revision": "816160e32e34b981b388",
+    "url": "/pamflet/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pamflet/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "816160e32e34b981b388",
+    "url": "/pamflet/static/js/main.7a0e0686.chunk.js"
+  },
+  {
+    "revision": "d28ce161812ba7f8a64d",
+    "url": "/pamflet/static/js/runtime~main.144b6568.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pamflet/static/media/logo.5d5d9eef.svg"
+  }
+]);
